9c5b1a7508db624fbe6adcbd0e7dac8711f3f8c3
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-03-22 22:39:13 +0530

    Remove HOC container pattern for shop page to add graphql

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,73 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-/* Styling */
-import './shoppage.styles.scss';
-
-/* Components */
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-/* Pages */
 import CollectionPage from '../collection/collection.component';
 
-/* Actions */
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-/* Selectors */
-import { selectIsFetchingCollections } from '../../redux/shop/shop.selectors';
-
-/* HOC's with spinner */
-const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
-
-const ShopPage = (props) => {
-  const { match, fetchCollectionsStartAsync, isFetchingCollections } = props;
-  /* Fetch shop data from firestore */
-  useEffect(() => {
-    fetchCollectionsStartAsync();
-  }, [fetchCollectionsStartAsync]);
-
-  return (
-    <div className="shop-page">
-      <Route
-        exact
-        path={`${match.path}`}
-        render={(props) => {
-          return (
-            <CollectionOverviewWithSpinner
-              isLoading={isFetchingCollections}
-              {...props}
-            />
-          );
-        }}
-      />
-      <Route
-        exact
-        path={`${match.path}/:collectionID`}
-        render={(props) => {
-          return (
-            <CollectionPageWithSpinner
-              isLoading={isFetchingCollections}
-              {...props}
-            />
-          );
-        }}
-      />
-    </div>
-  );
-};
-
-const mapStateToProps = createStructuredSelector({
-  isFetchingCollections: selectIsFetchingCollections,
-});
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default ShopPage;
